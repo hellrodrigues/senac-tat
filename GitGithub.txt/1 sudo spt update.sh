8b47704bb441b7b117d20a4356bdbdfa024ef7be
hel rodrigues <ellielolah@gmail.com>
2025-01-29 16:33:37 -0300

    atualizacao

---
+++ GitGithub.txt/1 sudo spt update.sh
@@ -0,0 +1,44 @@
+ 1  sudo spt update
+    2  sudo apt update
+    3  sudo apt upgrade 
+    4  sudo apt --fix-missing
+    5  sudo apt install
+    6  sudo apt upgrade 
+    7  sudo apt upgrade --fix-missing
+    8  sudo
+    9  subo apt update
+   10  sudo apt update
+   11  sudo apt upgrade
+   12  sudo apt upgrade --fix-missig
+   13  sudo apt upgrade --fix-missing
+   14  sudo apt autoremove
+   15  sudo apt autoclean
+   16  sudo reboot
+   17  sudo apt install curl
+   18  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+   19  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg 
+   20  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list~
+   21  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+   22  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+   23  sudo apt update
+   24  sudo apt install brave-browser
+   25  sudo apt install chromium
+   26  sudo aptupdate
+   27  sudo apt install git vim
+   28  sudo apt install kolourpaint notepadqq vlc audacity kazam
+   29  sudo app install kdenlive
+   30  sudo apt install linux-oem-24.04b
+   31  sudo reboot
+   32  git 
+   33  git config --global user.name "helrodrigues"
+   34  git config --global user.email 
+   35  git config --global user.name "hel rodrigues"
+   36  git config --global user.email 
+   37  git config --global user.email 
+   38  git config --global user.name "hel rodrigues 
+   39  git config --global user.email 
+   40  git config --global user.name "hel rodrigues"
+   41  git config --global user.email 
+   42  git config --list
+   43  exit
+   44  history
